49c1dca8e0b68a208b23a725b39d22d88751f25e
Nguyen Manh Ha <hanm@MacBook-Air-cua-nguyen.local>
2020-03-17 11:46:17 +0700

    add database

---
+++ sql/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace sql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var client = new SqlConnection("ConnectionString"))
+            {
+                client.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM DATABASE", client);
+            }
+        }
+    }
+}
